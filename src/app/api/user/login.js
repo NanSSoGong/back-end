921c0c5bfb41c8c7853f78c8a84a080305a7d662
daeuni <dek233@gmail.com>
2018-10-08 13:19:45 +0900

    Set Signin, Signup

--- src/app/api/user/login.js
+++ src/app/api/user/login.js
@@ -1,4 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto-promise');
 const db = require('../../module/pool.js');
 const jwt = require('../../module/jwt.js');
+
+router.post('/',async(req, res) =>{
+    let user_id = req.body.user_id;
+	let user_pw = req.body.user_pw;
+
+    if (!user_id || !user_pw) {
+		res.status(400).send({
+			message : "Null Value"
+		});
+	}else{
+		//user_id 로 index를 가져옴.
+		let checkQuery = 'SELECT * FROM user WHERE user_id = ?';
+		let checkResult = await db.execute2(checkQuery, [user_id]);	
+
+		if (!checkResult) {		//쿼리문 에러
+			res.status(500).send({
+				message : "Internal Server Error"
+			});
+		} else if (checkResult.length === 1) {		// user_id 가 존재하는 경우
+			let token = jwt.sign(checkResult[0].user_idx);
+			let hashedpwd = await crypto.pbkdf2(user_pw, checkResult[0].user_salt, 100000, 32, 'sha512');	// 입력받은 pw를 DB에 존재하는 salt로 hashing
+	        if (hashedpwd.toString('base64') === checkResult[0].user_pw) {	// hashed 된 비밀번호 비교
+    	        res.status(201).send({
+	                message: "Login Success",	//login 성공 
+					data : {
+						message : checkResult,
+						token : token
+					}
+	            });
+	        } else {
+	            res.status(400).send({
+	                message : "Login Failed"	// login pw 에러
+	            });
+	        }
+		} else {
+			res.status(400).send({
+				message : "Login Failed"	//login id 에러 
+			});
+			console.log("id error");
+    }
+}
+});
+
+module.exports = router;
---
+++ src/app/api/user/signup.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const crypto = require('crypto-promise');
+const db = require('../../module/pool.js');
+
+
+router.post('/', async (req, res) =>{
+    let user_name = req.body.user_name;
+    let user_phone = req.body.user_phone;
+    let user_email = req.body.user_email;
+    let user_id = req.body.user_id;
+    let user_pw = req.body.user_pw;
+
+    if (!user_id || !user_pw) {
+		res.status(400).send({
+			message : "Null Value"
+		});
+	} else {
+        let checkQuery = 'SELECT * FROM Cardit.User where user_id = ?';
+        let checkResult = await db.execute2(checkQuery, [user_id]);
+
+        if(!checkResult){
+            res.status(500).send({
+                mesaage : "Internal Server Error"
+            });
+        } else if(checkResult.length == 1){
+            res.status(400).send({
+                message : checkResult[0].user_id +" is already exist."
+            });
+        } else{
+            let salt = await crypto.randomBytes(32);
+            const hashedpwd = await crypto.pbkdf2(user_pw, salt.toString('base64'), 100000, 32, 'SHA512');
+            //정보 삽입 
+            let insertQuery = 'INSERT INTO user(user_name,user_phone,user_email,user_id, user_pw, user_salt) VALUES(?, ?, ?, ?,?,?)';
+            let insertResult = await db.execute2(insertQuery, [user_name,user_phone,user_email,user_id, hashedpwd.toString('base64'), salt.toString('base64')]);
+
+            if(!insertResult){
+                res.status(500).send({
+                    message : "Internal Server Error"
+                });
+            } else{
+                res.status(201).send({
+                    message : "Successfully Sign Up"
+                })
+            }
+        }
+    }
+})
+
+module.exports = router;

d0ce974d448062f7ac1b591b7dea3062d0d4df4c
daeuni <dek233@gmail.com>
2018-10-12 21:31:29 +0900

    Add Delete List

--- src/app/api/list/list.js
+++ src/app/api/list/list.js
@@ -6,9 +6,35 @@ const jwt = require('../../module/jwt.js');
 
 //get, put, post, delete
 
+//리스트 가져오기
+router.get('/:board_idx',async(req,res) =>{
+    const ID = jwt.verify(req.headers.authorization);
+    const board_idx = req.params.board_idx;
+
+    const getList = 'SELECT list_idx, list_name, list_position_x, list_position_y FROM CardIt.List WHERE board_idx = ?';
+
+    if(ID != -1){
+        const result = await db.execute2(getList,board_idx);
+        if(!result){
+            res.status(500).send({
+                message: "Internel Server Error"
+            });
+        }
+        else{
+            res.status(201).send({
+                message: "Successful Get List",
+                data : result
+            });
+        }
+    }else{
+        res.status(403).send({
+            message: 'Access Denied'
+        });
+    }
+});
+
 //리스트 등록하기
 router.post('/:board_idx',async(req, res) =>{
-    console.log('here');
     const ID = jwt.verify(req.headers.authorization);
 
     const board_idx = req.params.board_idx;
@@ -35,6 +61,34 @@ router.post('/:board_idx',async(req, res) =>{
             message: 'Access Denied'
         });
     }
+});
+
+//리스트 삭제하기
+router.delete('/:board_idx/:list_idx', async(req,res)=> {
+    const ID = jwt.verify(req.headers.authorization);
+    const board_idx = req.params.board_idx;
+    const list_idx = req.params.list_idx;
+
+    const deleteList = "DELETE FROM CardIt.List WHERE board_idx = ? AND list_idx = ?";
+
+    if(ID != -1){
+        const result = await db.execute3(deleteList,board_idx,list_idx);
+        if(!result){
+            res.status(500).send({
+                message: "Internel Server Error"
+            });
+        }
+        else{
+            res.status(201).send({
+                message: "Successful Delete List"
+            });
+        }
+    }else{
+        res.status(403).send({
+            message: 'Access Denied'
+        });
+    }
+
 });
 
 module.exports = router;

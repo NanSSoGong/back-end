2f297a8e6241a21531f5a92f135734ea8e572cd2
daeuni <dek233@gmail.com>
2018-12-02 17:22:00 +0900

    update cors

--- app.js
+++ app.js
@@ -14,17 +14,7 @@ var routes = require('./src/app/routes');
 var app = express();
 //cors
 var cors = require('cors');
-var whitelist = ['*'];
-var corsOptions = {
-  origin : function(origin, callback){
-    var isWhitelisted = whitelist.indexOf(origin) != -1;
-    callback(null, isWhitelisted);
-  },
-  credentials:true
-}
-
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet())
 
 // view engine setup
@@ -44,11 +34,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', routes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next(createError(404));
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE, LINK');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
 });
 
 // error handler

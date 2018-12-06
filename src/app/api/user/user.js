3cae6dfde238d6a7217c029317bdb2401ab9551d
YounggLee <youngg662@naver.com>
2018-12-06 20:54:26 +0900

    유저 검색 시 보드 공유되어 있는 유저 제외

--- src/app/api/user/user.js
+++ src/app/api/user/user.js
@@ -5,20 +5,22 @@ const db = require('../../module/pool.js');
 const jwt = require('../../module/jwt.js');
 
 //유저 가져오기
-router.post('/',async(req,res) =>{
+router.post('/:board_idx',async(req,res) =>{
     const ID = jwt.verify(req.headers.authorization);
 
     if(ID != -1){
+        const board_idx = req.params.board_idx;
         const user_id = req.body.user_id;
         let getUserQuery, result;
         if (!user_id || user_id == "") { //아이디가 입력되지 않은 경우 전체 검색
-            getUserQuery = 'SELECT user_idx, user_name, user_id, user_phone, user_email FROM CardIt.User;';
-            result = await db.execute1(getUserQuery);
+            //getUserQuery = 'SELECT user_idx, user_name, user_id, user_phone, user_email FROM CardIt.User;';
+            getUserQuery = 'SELECT T1.user_idx, T1.user_name, T1.user_id, T1.user_phone, T1.user_email FROM CardIt.User T1 LEFT JOIN (SELECT * FROM CardIt.Link WHERE board_idx = 1) T2 ON T1.user_idx = T2.user_idx WHERE T2.board_idx is null;';
         }
         else{ //아이디 전방 일치 검색
-            getUserQuery = "SELECT user_idx, user_name, user_id, user_phone, user_email FROM CardIt.User WHERE user_id LIKE '" + user_id + "%';";
-            result = await db.execute1(getUserQuery);
+            //getUserQuery = "SELECT user_idx, user_name, user_id, user_phone, user_email FROM CardIt.User WHERE user_id LIKE '" + user_id + "%';";
+            getUserQuery = "SELECT T1.user_idx, T1.user_name, T1.user_id, T1.user_phone, T1.user_email FROM CardIt.User T1 LEFT JOIN (SELECT * FROM CardIt.Link WHERE board_idx = 1) T2 ON T1.user_idx = T2.user_idx WHERE T2.board_idx is null AND T1.user_id LIKE '" + user_id + "%';";
         }
+        result = await db.execute1(getUserQuery);
 
         if(!result){
             res.status(500).send({
@@ -56,9 +58,9 @@ router.put('/share/:board_idx', async(req,res)=>{
             user_idx.forEach(user => {
                 values.push([user, board_idx_int, 0]);
             });
-            values.forEach(val => {
-                console.log(val);
-            });
+            // values.forEach(val => {
+            //     console.log(val);
+            // });
 
             const getListQuery = 'SELECT * FROM CardIt.Board WHERE board_idx = ?';
             const getList = await db.execute2(getListQuery, board_idx);
